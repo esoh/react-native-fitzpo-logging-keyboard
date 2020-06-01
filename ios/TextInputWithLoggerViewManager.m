d5eae02ea3dbaa5f11f5e7730c66ddcb9e86fb7d
Sean Oh <sean.so.oh@gmail.com>
2020-06-01 11:48:59 -0700

    feat(ios): connect custom component to custom input

--- ios/TextInputWithLoggerViewManager.m
+++ ios/TextInputWithLoggerViewManager.m
@@ -1,8 +1,26 @@
 #import "TextInputWithLoggerViewManager.h"
+#import "LoggingKeyboardView.h"
 #import <React/RCTSinglelineTextInputView.h>
 
 @implementation TextInputWithLoggerViewManager
 
 RCT_EXPORT_MODULE(TextInputWithLogger)
+
+- (UIView *)view
+{
+  // initialize textinput view that we are going to customize
+  RCTSinglelineTextInputView *textInputView = [[RCTSinglelineTextInputView alloc] initWithBridge:self.bridge];
+  // initialize custom keyboard input view
+  LoggingKeyboardView* customizedInputView= [[LoggingKeyboardView alloc] init];
+
+  // grab textview from textinput view and attach custom keyboard input view to it
+  UITextView* textView = (UITextView*)textInputView.backedTextInputView;
+  [textView setInputView:customizedInputView];
+
+  // add reference to textview from inputview so input knows where to send its actions
+  [customizedInputView setTextView:textView];
+
+  return textInputView;
+}
 
 @end
